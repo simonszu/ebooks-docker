e4c1ff9b6f2a6886f3ed15e595631742e3ba8e86
Simon Szustkowski <mail@simonszu.de>
2018-04-21 15:36:32 +0200

    Better initialization

--- run.sh
+++ run.sh
@@ -7,18 +7,16 @@ fi
 
 if [ ! -f /config/ebooksrc ]; then
   cp /template/ebooksrc /config/ebooksrc
-else
-  cp /config/ebooksrc /root/.ebooksrc
 fi
-
-
 
 # Copy bot to mounted app folder if an empty folder is mounted
 if [ ! -f /app/bots.rb ]; then
   cp /ebooks/bots.rb /app/bots.rb
-else # If there is a bot in the app folder, copy it to the executable location
-  cp -f /app/bots.rb /ebooks/bots.rb
 fi
+
+# Copy modified config and bot to their appropriate location
+cp /config/ebooksrc /root/.ebooksrc
+cp /app/bots.rb /ebooks/bots.rb
 
 # Generate the crontab
 cat <<EOF > /etc/cron.d/cron
