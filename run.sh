b385dd789ffe2f21ef55c344725a0c8976c0cbbd
Simon Szustkowski <mail@simonszu.de>
2018-04-21 14:35:45 +0200

    Removed cron debug stuff

--- run.sh
+++ run.sh
@@ -26,7 +26,6 @@ GEM_HOME=/usr/local/bundle
 
 30 * * * * root cd /ebooks; ebooks archive $USER >> /var/log/cron.log 2>&1
 45 * * * * root cd /ebooks; ebooks consume corpus/$USER.json >> /var/log/cron.log 2>&1
-* * * * * root echo "Hallo" >> /var/log/cron.log 2>&1
 # An empty line is required at the end of this file for a valid cron file
 
 EOF
